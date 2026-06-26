72da0f40062a0b4d5d6ac5be302eeda8f4a08fb8
Ansam Hassuna <hassunaansam@gmail.com>
2026-06-26 19:33:50 +0200

    Implement dark mode

--- js/script.js
+++ js/script.js
@@ -4,3 +4,32 @@ const navbar = document.querySelector(".navbar");
 menuBtn.addEventListener("click", () => {
     navbar.classList.toggle("active");
 });
+
+const themeBtn = document.getElementById("theme-toggle");
+const themeIcon = themeBtn.querySelector("i");
+
+// استرجاع الوضع المحفوظ
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark-mode");
+    themeIcon.classList.replace("fa-moon","fa-sun");
+}
+
+themeBtn.addEventListener("click",()=>{
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+
+        localStorage.setItem("theme","dark");
+
+        themeIcon.classList.replace("fa-moon","fa-sun");
+
+    }else{
+
+        localStorage.setItem("theme","light");
+
+        themeIcon.classList.replace("fa-sun","fa-moon");
+
+    }
+
+});
